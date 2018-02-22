ef537a86a9addea52794a9868b7e2f44d62a6a90
Michael Cavalea <callmecavs@gmail.com>
2018-02-22 17:35:50 -0500

    make it work

--- src/deep-access.js
+++ src/deep-access.js
@@ -1,8 +1,35 @@
-const deep = (obj, keys, value) => {
-  const pieces = Array.isArray(keys)
-    ? keys
-    : keys.split('.')
+const deep = (obj, dots, value) => {
+  // dot notation string -> array of keys
+  const keys = Array.isArray(dots)
+    ? dots
+    : dots.split('.')
 
+  // length of keys array
+  const len = keys.length
+
+  // run through keys
+  // store reference to object
+  let current = obj
+
+  // loop vars
+  let index
+  let key
+
+  for (index = 0; index < len; index++) {
+    // current key name
+    key = keys[index]
+
+    // make key an empty object if it doesnt exist
+    if (!current[key]) {
+      current[key] = {}
+    }
+
+    // update reference
+    current = current[key]
+  }
+
+  // TODO: need to either read & return || write & return here
+  console.log(current)
 }
 
 export default deep
