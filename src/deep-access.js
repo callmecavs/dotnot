c61b390280b2dd70c76ef5a9ad66ad96323cf87d
Michael Cavalea <callmecavs@gmail.com>
2018-02-22 19:57:14 -0500

    working get functions

--- src/deep-access.js
+++ src/deep-access.js
@@ -1,3 +1,44 @@
+const get = (obj, str, val) => {
+  // dot notation string -> array of keys
+  const keys = Array.isArray(str)
+    ? str
+    : str.split('.')
+
+  // length of keys array
+  const len = keys.length
+
+  // run through keys
+  // store reference to object
+  let current = obj
+
+  // loop vars
+  let index
+  let key
+
+  for (index = 0; index < len; index++) {
+    // current key name
+    key = keys[index]
+
+    // exit early, if the key doesnt exist
+    if (!current[key]) {
+      return
+    }
+
+    // update reference
+    current = current[key]
+  }
+
+  return current
+}
+
+export { get }
+
+const set = (obj, str, val) => {
+
+}
+
+export { set }
+
 const deep = (obj, dots, value) => {
   // dot notation string -> array of keys
   const keys = Array.isArray(dots)
