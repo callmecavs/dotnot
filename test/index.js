556b70c319fef9aca43501d3333e831f958d0e49
Michael Cavalea <callmecavs@gmail.com>
2018-02-22 20:07:50 -0500

    nice lil passing test suit

--- test/index.js
+++ test/index.js
@@ -1,9 +1,41 @@
+const access = require('../dist/deep-access.js')
 const expect = require('chai').expect
+
+const TEST = {
+  a: {
+    b: {
+      c: {
+        d: 1
+      }
+    }
+  },
+  e: 1
+}
 
 /* eslint-env mocha */
 
 describe('deep-access', () => {
-  it('should have tests', () => {
-    expect(true).to.equal(true)
+  it('should read 1 level deep', () => {
+    const expected = {
+      c: {
+        d: 1
+      }
+    }
+
+    expect(access.get(TEST, 'a.b')).to.deep.equal(expected)
+  })
+
+  it('should read 2 levels deep', () => {
+    const expected = {
+      d: 1
+    }
+
+    expect(access.get(TEST, 'a.b.c')).to.deep.equal(expected)
+  })
+
+  it('should read 3 levels deep', () => {
+    const expected = 1
+
+    expect(access.get(TEST, 'a.b.c.d')).to.deep.equal(expected)
   })
 })
